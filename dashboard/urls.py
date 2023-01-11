1c307bc9ba1e363ad73b9efc6efe62d329c37343
Dimas <dimas.ciputra@gmail.com>
2023-01-11 20:41:46 +0700

    Add kartoza map

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from dashboard.views import DashboardView
+from dashboard.views import DashboardView, SpaceView
 
 
 urlpatterns = [
     path('', DashboardView.as_view(), name='dashboard'),
+    path('space', SpaceView.as_view(), name='space'),
 ]
