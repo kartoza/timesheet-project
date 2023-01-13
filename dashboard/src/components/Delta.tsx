df41a754b368ed3b6ab0e8831c68bc72ef0293e8
Dimas <dimas.ciputra@gmail.com>
2023-01-13 12:25:03 +0700

    Fix show size

---
+++ dashboard/src/components/Delta.tsx
@@ -0,0 +1,4 @@
+import { Quill } from 'react-quill';
+const Delta = Quill.import('delta')
+
+export default Delta;
--- dashboard/src/components/ReactQuill.tsx
+++ dashboard/src/components/ReactQuill.tsx
@@ -12,13 +12,14 @@ import Filter8Icon from '@mui/icons-material/Filter8';
 import React, {useEffect, Suspense, useState, useCallback, useRef} from 'react';
 import 'react-quill/dist/quill.snow.css';
 import Loader from '../loadable/Loader';
-import '../styles/Quill';
+import '../styles/Quill.scss'
 import ListItemIcon from '@mui/material/ListItemIcon';
 
 const ReactQuill = React.lazy(() => import('react-quill'))
 const Popover = React.lazy(() => import('@mui/material/Popover'))
 
-
+import { Quill } from 'react-quill';
+const Delta = Quill.import('delta')
 
 const SIZES = [
     {
@@ -62,6 +63,28 @@ export default function TReactQuill(props: any) {
     const quillContainer = useRef(null)
     const [itemSelected, setItemSelected] = useState<number>(0)
 
+    useEffect(() => {
+        if (open) {
+            setItemSelected(1)
+        }
+    }, [open])
+
+    useEffect(() => {
+        if (quill.current) {
+            const _quill = (quill as any).current.getEditor();
+            _quill.clipboard.addMatcher(Node.ELEMENT_NODE, (node: any, delta: any) => {
+                return delta.compose(new Delta().retain(delta.length(), 
+                { color: false,
+                    background: false,
+                    bold: false,
+                    strike: false,
+                    underline: false
+                }
+                ));
+              })
+        }
+    }, [quill.current])
+
     const onChange = useCallback((value: any) => {  
         // Find the last colon
         if (!quill.current || !quillContainer.current) {
@@ -71,19 +94,29 @@ export default function TReactQuill(props: any) {
         const container: any = (quillContainer.current as any)
         const containerBound = container.getBoundingClientRect()
         const sel: any = editor.getSelection()
-        const bound: any = editor.getBounds(sel.index)
-
-        setAnchorPosition({
-            top: containerBound.top + bound.top,
-            left: containerBound.left + bound.left
-        })
-        let re = new RegExp(`(${KEY_WORD_REGEX}<\/()>?(\\w+[<\/(\\w+)>]+))$`, "g");
-        if (value.match(re)) {
-            setItemSelected(1)
-            setOpen(true)
+        if (sel) {
+            const bound: any = editor.getBounds(sel.index)
+            setAnchorPosition({
+                top: containerBound.top + bound.top,
+                left: containerBound.left + bound.left
+            })
+            let re = new RegExp(`(${KEY_WORD_REGEX}<\/()>?(\\w+[<\/(\\w+)>]+))$`, "g");
+            if (value.match(re)) {
+                setOpen(true)
+                setTimeout(() => {
+                    editor.focus()
+                }, 100)
+            }
+        }
+        if (open) {
+            if (value.match(/<br>/g)) {
+                if (!props.value.match(/<br>/g) || value.match(/<br>/g).length > props.value.match(/<br>/g).length) {
+                    return
+                }
+            }
         }
         props.onChange(value)
-    }, [quill, quillContainer])
+    }, [quill, quillContainer, open])
 
     const addSize = (index = null) => {
         try {
@@ -112,6 +145,8 @@ export default function TReactQuill(props: any) {
             } else if (e.keyCode === 13) {
                 e.preventDefault();
                 e.stopPropagation();
+                const editor: any = (quill.current as any).getEditor()
+                editor.setSelection(editor.getSelection().index - 1, 0)
                 addSize()
                 setOpen(false)
             } else {
@@ -141,8 +176,6 @@ export default function TReactQuill(props: any) {
                 open={open}
                 anchorReference="anchorPosition"
                 anchorPosition={anchorPosition}
-                disableEnforceFocus
-                hideBackdrop
                 onClick={handlePopoverClick}
                 onKeyDown={handleKeyDown}
                 anchorOrigin={{
@@ -172,7 +205,6 @@ export default function TReactQuill(props: any) {
             <div ref={quillContainer}>
                 <ReactQuill
                     ref={quill}
-                    readOnly={open}
                     formats={props.format}
                     modules={props.modules}
                     theme='snow'
