26456c5c26e0ad0d6069d1d4bc5bf5340e4d1e38
Dimas <dimas.ciputra@gmail.com>
2023-11-30 12:20:19 +0700

    Fix countdown calculation

--- timesheet/management/commands/update_erp_data.py
+++ timesheet/management/commands/update_erp_data.py
@@ -2,7 +2,8 @@ import logging
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand
 
-from schedule.api_views.schedule import calculate_remaining_task_days, update_previous_schedules
+from schedule.api_views.schedule import calculate_remaining_task_days, update_previous_schedules, \
+    update_subsequent_schedules, _naive
 from schedule.models import Schedule
 from timesheet.utils.erp import (
     pull_projects_from_erp, pull_leave_data_from_erp
@@ -27,21 +28,43 @@ class Command(BaseCommand):
             updated_tasks.append(
                 schedule.task.id
             )
+            start_time = _naive(schedule.start_time)
+            end_time = _naive(schedule.end_time)
+            last_update = _naive(schedule.task.last_update)
+
             remaining_task_days = calculate_remaining_task_days(
                 schedule.task,
-                schedule.start_time,
-                schedule.end_time
+                start_time,
+                end_time
             )
             print('updating schedule {user} : {task} - {remaining_days}'.format(
                 user=user.username,
                 task=schedule.task.name,
                 remaining_days=remaining_task_days
             ))
-            update_previous_schedules(
-                start_time=schedule.start_time,
-                task_id=schedule.task.id,
-                remaining_days=remaining_task_days
+            last_day_number = (
+                    remaining_task_days - (schedule.end_time - schedule.start_time).days
             )
+            schedule.first_day_number = remaining_task_days
+            schedule.last_day_number = last_day_number
+            schedule.save()
+
+            updated = update_subsequent_schedules(
+                start_time=start_time,
+                task_id=schedule.task.id,
+                last_day_number=last_day_number,
+                excluded_schedule=schedule
+            )
+            if start_time < last_update:
+                excluded_schedules = updated
+                excluded_schedules.append(schedule.id)
+                updated_previous = update_previous_schedules(
+                    start_time,
+                    schedule.task.id,
+                    remaining_task_days,
+                    excluded_schedules=excluded_schedules
+                )
+                updated += updated_previous
 
     def handle(self, *args, **options):
         users = get_user_model().objects.all()
