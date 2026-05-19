d63719f271229fbae10c14a21a600766c891aa4d
Jeff Osundwa <jeff@kartoza.com>
2026-05-19 14:20:38 +0300

    feat: Add login requirement to PMO Dashboard view

--- pmo_dashboard/views.py
+++ pmo_dashboard/views.py
@@ -1,5 +1,7 @@
+
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 
-class PMODashboardView(TemplateView):
+class PMODashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'pmo_dashboard/dashboard.html'
